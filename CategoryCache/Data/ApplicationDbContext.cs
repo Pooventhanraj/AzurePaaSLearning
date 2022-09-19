d1052b037aa392dbd358fdb3255627925f3eaeb1
Pooventhanraj <lpooventhanraj@gmail.com>
2022-09-20 01:57:30 +0530

    Redis cache completed

---
+++ CategoryCache/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CategoryCache.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CategoryCache.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
